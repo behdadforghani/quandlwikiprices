147e6ace6be80fc2416f6de14fd400f49be31902
Behdad Forghani <behdad.forghani@gmail.com>
2018-01-11 22:28:06 -0600

    Fix checkpoint date.

--- src/main/scala/com/exapackets/DownloadEod.scala
+++ src/main/scala/com/exapackets/DownloadEod.scala
@@ -9,7 +9,8 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{ State, Time }
 import org.apache.spark.streaming.util.OpenHashMapBasedStateMap
 import org.apache.spark.util.Utils
-import org.apache.spark.sql.{ SparkSession, DataFrame}
+import org.apache.spark.sql.{ SparkSession, DataFrame }
+import org.apache.spark.sql.functions.{ max, col }
 
 import java.text.SimpleDateFormat
 import java.util.Properties
@@ -37,14 +38,12 @@ object DownloadEod {
   var dbPass: String = null
   val env = sys.env
 
-
-  
   def error(msg: String) {
     System.err.println(msg)
     LOG.error(msg)
     System.exit(1)
   }
-  
+
   def usage() {
     error("usage: DownloadEod [start-date]");
     System.exit(1)
@@ -59,26 +58,27 @@ object DownloadEod {
       .appName(APP_NAME)
       .getOrCreate()
 
-        val df = spark.read
-        .format("com.databricks.spark.csv")
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .option("delimiter",",").option("quote","")
-        .load(csvFile)
-        
-        // rename columns ex-dividend, date and open (reserved words) and open so that the users do
-        // not need to escape the column names 
-        val colMap = Map("ex-dividend" -> "ex_dividend", "open" -> "open_value", "date" -> "date_value")
-        
-        var dateFilteredDf:DataFrame = null
-        if(start != null) {
-          dateFilteredDf = df.filter(s"date > cast('${start}' as DATE)")
-        } else {
-          dateFilteredDf = df
-        }
-        val ndf = 
-        dateFilteredDf.select(df.columns.map(c => df.col(c).as(colMap.getOrElse(c, c))): _*)
-        ndf.show()
+    val df = spark.read
+      .format("com.databricks.spark.csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .option("delimiter", ",").option("quote", "")
+      .load(csvFile)
+
+    // rename columns ex-dividend, date and open (reserved words) and open so that the users do
+    // not need to escape the column names 
+    val colMap = Map("ex-dividend" -> "ex_dividend", "open" -> "open_value", "date" -> "date_value")
+
+    val dateFilteredDf = if (start != null) {
+      //          df.filter(s"date > cast('${start}' as DATE)")
+      println(s"date > '${start}'")
+      df.filter(s"date > cast('${start}' as TIMESTAMP)")
+    } else {
+      df
+    }
+    val ndf =
+      dateFilteredDf.select(df.columns.map(c => df.col(c).as(colMap.getOrElse(c, c))): _*)
+    ndf.show()
 
     val connectionProperties = new Properties()
     connectionProperties.put("user", dbUser)
@@ -86,15 +86,24 @@ object DownloadEod {
     connectionProperties.put("driver", "org.postgresql.Driver")
     ndf.write.mode("append").jdbc(jdbcUrl, dbTable, connectionProperties)
 
+    val maxDf = ndf.agg(max("date_value")).withColumn("m", col("max(date_value)").cast("DATE")).select("m").rdd.collect()
+    if (maxDf.length > 0) {
+      val maxDate = maxDf(0)(0)
+      println(s"maxDate=${maxDate}")
+      val pw = new PrintWriter(new File(checkPoint))
+      pw.write(s"${maxDate}")
+      pw.close
+    }
+
     sc.stop()
   }
-  
+
   def getEnv(key: String): String = {
     val envVar = env.getOrElse(key, null).asInstanceOf[String]
-    if(envVar == null) {
-      throw new Exception("Environment variable "+key+" has not been set")
+    if (envVar == null) {
+      throw new Exception("Environment variable " + key + " has not been set")
     }
-    
+
     envVar
   }
 
@@ -136,13 +145,7 @@ object DownloadEod {
     val inputCsv = QuandlRest.fetchData(key)
     println("fetched data, uploading data to database")
     loadData(inputCsv, start)
-//  For testing    
-//    loadData("WIKI_PRICES_212b326a081eacca455e13140d7bb9db.csv", start)
-    val now = DateTime.now
-    val tomorrowTime = now.plusDays(1)
-    val tomorrow = tomorrowTime.toString(dateFormat)
-    val pw = new PrintWriter(new File(checkPoint))
-    pw.write(tomorrow)
-    pw.close
+    //  For testing    
+    //        loadData("WIKI_PRICES_212b326a081eacca455e13140d7bb9db.csv", start)
   }
 }
